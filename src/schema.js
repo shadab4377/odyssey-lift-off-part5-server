eefbdb99254bcb3c2aca8a4beb0c9b4ef7679c75
Shadab Khan <shakhan25@publicisgroupe.net>
2022-08-24 18:09:15 +0530

    schema.js errors removed

--- src/schema.js
+++ src/schema.js
@@ -66,7 +66,7 @@ const typeDefs = gql`
     "The module's video duration, in seconds"
     durationInSeconds: Int
     "The module's length in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds")f
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
